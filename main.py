ab2c74e98e86ff82aeef082c0d8db9e427c2c941
Mauricio Sartori <omauricioguerrero@gmail.com>
2022-05-06 20:52:18 -0700

    Add DNA structure file

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'DNA Strand Visualizations, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    lines = tuple(open('dnaStructure.txt', 'r'))
+    for line in lines:
+        print_hi('Line says ' + line)
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
