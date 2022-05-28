4b188f0350a3345df1c829d460d9577e22fcde3f
Mauricio Laine <laine.mau@gmail.com>
2022-05-27 17:59:45 -0700

    Create double helix transition

--- sceneDouble.py
+++ sceneDouble.py
@@ -1,8 +1,7 @@
 from manim import *
 
-# Demo of using the manim library.
-# https://docs.manim.community/en/stable/tutorials/quickstart.html#overview
-# manim -pql scene.py SquareAndCircle
+# Double Strand animation
+# manim -pql sceneDouble.py DoubleStrand
 
 def get_input_strand(file_name):
     file = open("./assets/" + file_name, "r")
@@ -14,19 +13,26 @@ class DoubleStrand(Scene):
         axis = Axes()
         a = ValueTracker(-1 * PI)
 
-        # Initial animation 1 (strand)
+        # Initial animation - Strands interaction
         input_strand = get_input_strand('dnaParenDot_2.txt')
         input_strand = input_strand.translate(str.maketrans('', '', ' \n\t\r'))
         elements = input_strand.split("+")
         print(elements)
 
         #f(x) = sin(x + a + sin(x))
-        s1 = always_redraw(lambda : axis.plot(lambda x : np.sin(x + a.get_value()) + np.sin(x) + 1).set_color(GREEN))
-        s2 = always_redraw(lambda : axis.plot(lambda x : np.cos(x + a.get_value()) + np.cos(x) - 1).set_color(BLUE))
-        self.play(Create(VGroup(s1, s2)))
+        strand_1 = always_redraw(lambda : axis.plot(lambda x : np.sin(x + a.get_value()) + np.sin(x) + 1).set_color(RED))
+        #f(x) = cos(x + a + cos(x))
+        strand_2 = always_redraw(lambda : axis.plot(lambda x : np.cos(x + a.get_value()) + np.cos(x) - 1).set_color(BLUE))
+        self.play(Create(VGroup(strand_1, strand_2)))
         self.wait()
         #Value of "a" becomes (5 * PI & - 5 * PI)
         self.play(a.animate.set_value(5 * PI), run_time=10)
         self.wait()
         self.play(a.animate.set_value(-5 * PI), run_time=10)
+        self.wait()
+
+        # Final animation - Create double helix
+        helix_1 = axis.plot(lambda x : np.sin(x)).set_color(RED)
+        helix_2 = axis.plot(lambda x : np.sin(x+PI)).set_color(BLUE)
+        self.play(ReplacementTransform(strand_1, helix_1), ReplacementTransform(strand_2, helix_2))
         self.wait()
